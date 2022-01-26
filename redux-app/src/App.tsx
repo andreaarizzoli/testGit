d836ce6e0334ec686f094643f5b0246588d9395f
andrea arizzoli <andrea.arizzoli@ied.edu>
2022-01-26 11:13:27 +0100

    Merge Cecilia

--- redux-app/src/App.tsx
+++ redux-app/src/App.tsx
@@ -14,31 +14,20 @@ import { CreateTodoCecilia } from './features/todos/components/CreateTodoCecilia
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-=======
-      <BrowserRouter>
       
-      <Link to="/Cecilia">Cecilia</Link>
->>>>>>> features/44444/cecilia
-
       <BrowserRouter>
         <Link to='/Andrea'>Inserisci by Andrean</Link>
         <Link to={'/Michele'}>MicheleTodo</Link>
+        <Link to="/Cecilia">Cecilia</Link>
+
 
         
         <Routes>
           <Route path='/' element={<CreateTodo />}></Route>
-<<<<<<< HEAD
           <Route path='/Michele' element={<MicheleTodo />}></Route>
           <Route path='/Andrea' element={<CreateAndrea />}></Route>
           <Route path='/Manuela' element={<ManuTodo />}></Route>
-          <Route path='/Cecilia' element={<CreateTodo />}></Route>
-=======
-          <Route path='/Michele' element={<CreateTodo />}></Route>
-          <Route path='/Andrea' element={<CreateTodo />}></Route>
-          <Route path='/Manuela' element={<CreateTodo />}></Route>
           <Route path='/Cecilia' element={<CreateTodoCecilia />}></Route>
->>>>>>> features/44444/cecilia
           <Route path='/Maurizio' element={<CreateTodo />}></Route>
         </Routes>
         <TodoList></TodoList>
