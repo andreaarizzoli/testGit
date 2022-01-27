c97778672015fe11f2d80b1453adde89cf1d599e
Mauripiace92 <piacenzamaurizio@gmail.com>
2022-01-27 15:02:37 +0100

    parte grafica

--- redux-app/src/features/albums/components/Card.tsx
+++ redux-app/src/features/albums/components/Card.tsx
@@ -18,31 +18,30 @@ export const Card : FC<CardProps> = ({
    
    
     const Carditem = styled.div`
-    border-radius: 10px;
-    width: 30%;
-    margin: 20px;
-    box-shadow: 8px 8px 14px -6px rgba(0, 0, 0, 0.74);
-
-    .imgcontainer {
-      
-      height: 54%;
       border-radius: 10px;
-    }
+      width: 40%;
+      margin: 20px;
+      box-shadow: 8px 8px 14px -6px rgba(0, 0, 0, 0.74);
+      background-color:#C9D0D4;
+       
+      .imgcontainer {
+        height: 54%;
+        border-radius: 10px;
+      }
 
-    img {
-      width: 100%;
-      height: 100%;
-      border-radius: 10px;
-      object-fit: initial;
-    }
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 10px;
+        object-fit: initial;
+      }
 
-    .Cardfooter {
-      background-color: rgba(255, 255, 255, 0.2);
-      padding: 10px 0;
-      display: ${onToggle ? 'block': 'none'};
-      transition: 0.3s;
-    }
-  `;
+      .Cardfooter {
+        padding: 10px 0;
+        display: ${onToggle ? "block" : "none"};
+        transition: 0.5s;
+      }
+    `;
 
 
     const Button = styled.button`
@@ -51,15 +50,16 @@ export const Card : FC<CardProps> = ({
       padding: 0.5rem 0;
       margin: 0.5rem 1rem;
       width: 9rem;
-      background: transparent;
-      color: black;
-      border: 2px solid black;
+      background: #068de6;
+      color: white;
+      border: 2px solid white;
       cursor: pointer;
 
-      button:hover {
-        background-color: black;
-        color: white;
-        
+      :hover {
+        transition: .5s;
+        background-color: white;
+        color: #068de6;
+        border: 2px solid #068de6;
       }
     `;
 
@@ -76,7 +76,8 @@ export const Card : FC<CardProps> = ({
         </div>
           <h2>UserId: {userId}</h2>
         <div className="Cardfooter">
-          <h5> Title: {title}</h5>
+          <h5> Title</h5>
+          <p>{title}</p>
         </div>
         <Button onClick={toggle}>{onToggle ? 'Vedi Meno' : 'Vedi Dettagli'}</Button>
       </Carditem>
